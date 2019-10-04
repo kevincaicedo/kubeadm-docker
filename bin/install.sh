c2e79850f275a78d33c8ffaeec65f21302651443
Kevin Estiben Caicedo Riascos <kcaicedo@coopebombas.com>
2019-10-04 08:12:50 -0500

    changed to flannel

--- bin/install.sh
+++ bin/install.sh
@@ -41,9 +41,11 @@ systemctl enable kubelet
 
 if [ "$1" = "master" ];
 then
-    kubeadm init --pod-network-cidr=192.168.0.0/16
+    kubeadm init --pod-network-cidr=10.244.0.0/16
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
-    kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+
+    sysctl net.bridge.bridge-nf-call-iptables=1
+    kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
 fi
